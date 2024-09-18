ae1b8e469e8cc36b2e66107cf823f20958fa809c
pakin <pakin.dkw@gmail.com>
2024-09-18 21:31:20 +0700

    แก้ Model itemIssue

--- models/itemIssue.js
+++ models/itemIssue.js
@@ -4,8 +4,6 @@ const mongoose = require('mongoose');
 const itemIssueSchema = new mongoose.Schema({
     tag_id:{type:String},
     iss_qty:{type:Number},
-    iss_actual_date:{type:Date},
-    iss_expected_date:{type:Date},
     issue_order_id:{type:String, required:true},
 
     
