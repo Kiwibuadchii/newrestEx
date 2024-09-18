6ebed0d5675077f589db82583811492839d72a44
pakin <pakin.dkw@gmail.com>
2024-09-18 14:25:03 +0700

    แก้ origin

--- app.js
+++ app.js
@@ -75,7 +75,7 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 const corsOptions = {
-  origin: 'https://newrestex-production.up.railway.app',
+  origin: 'http://localhost:4200',
   credentials: true,
   methods: "GET, POST, PUT, DELETE",
 };
