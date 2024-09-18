d7a33669112bf2f017776c6c4d05ff4f38d7502f
pakin <pakin.dkw@gmail.com>
2024-09-18 20:45:05 +0700

    แก้ model production order detail

--- models/productionOrderDetail.js
+++ models/productionOrderDetail.js
@@ -3,7 +3,6 @@ const mongoose = require('mongoose');
 
 const productionDetailSchema = new mongoose.Schema({
     prod_code:{type:String},
-    expected_daate:{type:Date},
     production_order_id:{type:String},
     iss_qty:{type:Number},
 })
